f37fad64c63a7d12c20007128eb2a852a2985392
汪开辉 <hpuwang@gmail.com>
2020-03-11 21:24:15 +0800

    swg

--- server/src/Bundle/AppBundle/Controller/UserController.php
+++ server/src/Bundle/AppBundle/Controller/UserController.php
@@ -5,8 +5,9 @@ namespace App\Bundle\AppBundle\Controller;
 use App\Bundle\CenterBundle\Lib\Base\BaseController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\View as ViewAnnotations;
-use FOS\RestBundle\View\View;
-use FOS\RestBundle\Context\Context;
+use Swagger\Annotations as SWG;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -17,21 +18,43 @@ class UserController extends BaseController
     /**
      * @Rest\Post("/login")
      * @ViewAnnotations()
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returned when the register is successful",
+     *     @SWG\Schema(
+     *         type="object",
+     *         example={"name":"wang-v1.0"}
+     *     )
+     * ),
+     * @SWG\Parameter(
+     *     name="X-Accept-Version",
+     *     in="header",
+     *     type="string",
+     *     description="X-Accept-Version",
+     *     required=true
+     * ),
+     *
+     * @SWG\Parameter(
+     *     name="login api",
+     *     in="body",
+     *     type="string",
+     *     description="user login",
+     *     required=true,
+     *     @SWG\Schema(type="object",
+     *          @SWG\Property(property="testName", description="testName", type="string"),
+     *          required={
+     *               "testName"
+     *          }
+     *     ),
+     * )
+     * @SWG\Tag(name="Login")
      */
     public function login(Request $request)
     {
         $name = $request->get("testName");
         $version = $request->headers->get("X-Accept-Version");
         $data=["name"=>$name."-".$version];
-
-        $view = View::create();
-        $context = new Context();
-        $context->setVersion('1.0');
-        $context->addGroup('user');
-        $view->setContext($context);
-
-        $view->setData($data)->setTemplateData($data);
-
-        return $view ;
+        return $data;
     }
 }
