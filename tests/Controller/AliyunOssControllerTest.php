29607a38678d30dc74938c8ec1f543d7438941e0
汪开辉 <hpuwang@gmail.com>
2020-05-19 11:17:45 +0800

    debug

---
+++ tests/Controller/AliyunOssControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AliyunOssTest extends WebTestCase
+{
+    public function testSomething()
+    {
+
+    }
+}
---
+++ tests/WebTestCaseBase.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @Author: kaihui.wang
+ * @Contact  hpuwang@gmail.com
+ * @Version: 1.0.0
+ * @Date: 2020/5/19 11:02
+ */
+class WebTestCaseBase
+{
+
+}
