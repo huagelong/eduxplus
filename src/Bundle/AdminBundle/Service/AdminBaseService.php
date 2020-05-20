d8fbdf4e0e549b49352c9745a9eaedd238814ee4
汪开辉 <hpuwang@gmail.com>
2020-05-20 17:18:06 +0800

    api 权限检查

---
+++ src/Bundle/AdminBundle/Service/AdminBaseService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author: kaihui.wang
+ * @Contact  hpuwang@gmail.com
+ * @Version: 1.0.0
+ * @Date: 2020/5/20 16:20
+ */
+
+namespace App\Bundle\AdminBundle\Service;
+
+
+class AdminBaseService
+{
+
+}
---
+++ src/Bundle/ApiBundle/Service/ApiBaseService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author: kaihui.wang
+ * @Contact  hpuwang@gmail.com
+ * @Version: 1.0.0
+ * @Date: 2020/5/20 16:10
+ */
+
+namespace App\Bundle\ApiBundle\Service;
+
+
+class ApiBaseService
+{
+
+}
