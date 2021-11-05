e5090d5fadb05d1b9d25f15a5f7a68685e149a55
wangkaihui <hpuwang@gmail.com>
2021-11-05 22:01:34 +0800

    去掉不需要的

--- docs/k8s/code/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir /www
-cp -a /var/www/symfony/. /www
-tail -f /dev/null
--- k8s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t eduxplus_code -f docs/k8s/code/Dockerfile .
-docker build -t eduxplus_nginx -f docs/k8s/nginx/Dockerfile .
-docker build -t eduxplus_php -f docs/k8s/php-fpm/Dockerfile .
