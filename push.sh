ac70bdf147080ec2499c29687f0162c676894ca4
wangkaihui <hpuwang@gmail.com>
2021-11-13 22:19:13 +0800

    修改基础git仓库

--- push.sh
+++ push.sh
@@ -2,16 +2,16 @@ echo '开始推送github'
 echo '切换git地址'
 git remote rm origin
 
-git remote add origin git@gitee.com:wangkaihui/eduxplus.git
+git remote add origin git@github.com:trensy/eduxplus.git
 echo '设置上传代码分支，推送gitee'
 git push --set-upstream origin master --force
 echo '推送github完成'
 git remote rm origin
 
-git remote add origin git@github.com:trensy/eduxplus.git
+git remote add origin git@gitee.com:wangkaihui/eduxplus.git
 
 git pull origin master
 
-echo '切回github资源'
+echo '切回gitee资源'
 git branch --set-upstream-to=origin/master master
 echo '设置git跟踪资源'
