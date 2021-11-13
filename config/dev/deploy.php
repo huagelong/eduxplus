77ce08289e7d9ce2bcd631043dc126db12a91be4
wangkaihui <hpuwang@gmail.com>
2021-11-13 22:22:25 +0800

    修改基础git仓库

---
+++ config/dev/deploy.php
@@ -0,0 +1,37 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
+            ->server('root@dev.eduxplus.com')
+            // the absolute path of the remote server directory where the project is deployed
+            ->deployDir('/www/wwwroot/dev.eduxplus.com')
+            // the URL of the Git repository where the project code is hosted
+            ->repositoryUrl('git@github.com:trensy/eduxplus.git')
+            // the repository branch to deploy
+            ->repositoryBranch('master')
+            ->symfonyEnvironment("dev")
+            ->installWebAssets(false)
+            ->keepReleases(2);
+        ;
+    }
+
+    // run some local or remote commands before the deployment is started
+    public function beforeStartingDeploy()
+    {
+        $this->log ( '准备应用' );
+        $this->runLocal ( 'SYMFONY_ENV=dev composer deploy' );
+    }
+
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+//        $this->runRemote('{{ console_bin }} app:my-task-name');
+         $this->runLocal('say "The deployment has finished."');
+    }
+};
