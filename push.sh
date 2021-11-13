4093521496c4ec56ca6f5b60d06ce11462577a0b
wangkaihui <hpuwang@gmail.com>
2021-11-13 22:20:35 +0800

    修改基础git仓库

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ echo '切换git地址'
 git remote rm origin
 
 git remote add origin git@github.com:trensy/eduxplus.git
-echo '设置上传代码分支，推送gitee'
+echo '设置上传代码分支，推送github'
 git push --set-upstream origin master --force
 echo '推送github完成'
 git remote rm origin
