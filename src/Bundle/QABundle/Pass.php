36f265a4849ccfeda843a2ed45cccde65fdb9d6d
wangkaihui <hpuwang@gmail.com>
2021-12-16 18:38:12 +0800

    symfony升级

--- src/Bundle/QABundle/Pass.php
+++ src/Bundle/QABundle/Pass.php
@@ -8,8 +8,6 @@
 
 namespace App\Bundle\QABundle;
 
-
-use Symfony\Bundle\TwigBundle\Loader\NativeFilesystemLoader;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -19,9 +17,6 @@ class Pass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $twigFilesystemLoaderDefinition = $container->getDefinition('twig.loader.native_filesystem');
-        if ($container->getParameter('kernel.debug')) {
-            $twigFilesystemLoaderDefinition->setClass(NativeFilesystemLoader::class);
-        }
         $twigFilesystemLoaderDefinition->addMethodCall('addPath', array(__DIR__."/Resources/templates/admin", 'QABundleAdmin'))
             ->addMethodCall('addPath', array(__DIR__."/Resources/templates/default", 'QABundle'))
         ;
