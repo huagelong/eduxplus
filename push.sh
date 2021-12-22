a2b3c1fe29a49c3afa9b853f7a2dfa20617cb70c
kaihui.wang <hpuwang@gmail.com>
2021-12-22 09:11:58 +0800

    推送脚本更新添加teambition

--- push.sh
+++ push.sh
@@ -5,20 +5,17 @@ echo '设置上传代码分支，推送github'
 git push --set-upstream origin master --force
 echo '推送github完成'
 
-
-echo '开始推送gitee'
-git remote rm origin
-git remote add origin git@gitee.com:wangkaihui/eduxplus.git
-git push --set-upstream origin master --force
-
-
 echo '开始推送teambition'
 git remote rm origin
 git remote add origin git@codeup.teambition.com:trensy/eduxplus.git
 git push --set-upstream origin master --force
 
+echo '开始推送gitee'
+git remote rm origin
+git remote add origin git@gitee.com:wangkaihui/eduxplus.git
+git push origin master
 
-echo '切回teambition资源'
+echo '切回gitee资源'
 git branch --set-upstream-to=origin/master master
 git push
 echo '设置git跟踪资源'
