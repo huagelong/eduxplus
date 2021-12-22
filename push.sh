f12c4f8a5f438a48dc225d417915ec0d03a8d6b6
wangkaihui <hpuwang@gmail.com>
2021-12-22 08:46:04 +0800

    推送

--- push.sh
+++ push.sh
@@ -1,18 +1,24 @@
 echo '开始推送github'
-echo '切换git地址'
 git remote rm origin
-
 git remote add origin git@github.com:trensy/eduxplus.git
 echo '设置上传代码分支，推送github'
 git push --set-upstream origin master --force
 echo '推送github完成'
+
+
+echo '开始推送gitee'
 git remote rm origin
-
 git remote add origin git@gitee.com:wangkaihui/eduxplus.git
+git push --set-upstream origin master --force
 
-git pull origin master
 
-echo '切回gitee资源'
+echo '开始推送teambition'
+git remote rm origin
+git remote add origin git@codeup.teambition.com:trensy/eduxplus.git
+git push --set-upstream origin master --force
+
+
+echo '切回teambition资源'
 git branch --set-upstream-to=origin/master master
 git push
 echo '设置git跟踪资源'
